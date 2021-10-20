ff58b0c8928776d67bd2d03ba0c204289d36884d
AslonZ <slothzz@qq.com>
2021-10-20 22:19:35 +0800

    Initial Commit

---
+++ 学习swiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  学习swiftUI
+//
+//  Created by 米汇科技 on 2021/10/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 学习swiftUI/__swiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  __swiftUIApp.swift
+//  学习swiftUI
+//
+//  Created by 米汇科技 on 2021/10/20.
+//
+
+import SwiftUI
+
+@main
+struct __swiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
